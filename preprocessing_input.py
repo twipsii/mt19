f4a04d14c53dce3df7408b998d71b0968994921c
twipsii <twipsy92@hotmail.com>
2019-04-27 12:27:35 +0200

    Update preprocessing_input.py

--- preprocessing_input.py
+++ preprocessing_input.py
@@ -9,14 +9,17 @@ import os
 import argparse
 
 def preprocess_text(input_filename, output_filename):
+    """Pre-processes the text given an input file and stores processed text in an output file."""
+    
+    # define output file
     if not output_filename:
         output_filename = os.path.abspath(input_filename) + ".prep"
+    # read input file
     with codecs.open(input_filename, "r", "UTF-8") as f:
         lines = f.readlines()
-    
-    lines = [line.strip() for line in lines if line.strip() != ""]
-    lines = [line.strip().lower() for line in lines]
-    
+    # remove empty lines, strip leading and trailing whitespaces and lowercase the text
+    lines = [line.strip().lower() for line in lines if line.strip() != ""]
+    # write to output file
     with codecs.open(output_filename, "w+", "UTF-8") as f:
         for line in lines:
             f.write(line + "\n")
